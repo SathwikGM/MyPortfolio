d9457269593e5774495b4b0e1a7d0c55f2a5e6b1
sathwikgm <sathwikgm14@gmail.com>
2023-02-24 22:02:37 +0530

    changes to projects component

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,9 +1,51 @@
 import "./projects.scss";
 
+const projects = [
+  {
+    name: "My Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quibusdam, doloremque repudiandae ratione autem tenetur illo. Repellendus eius tempora dolore similique, quia reprehenderit harum repudiandae molestiae ad sit atque odit.",
+    pageUrl: "http://localhost:3000/about",
+    gitHubUrl: "https://github.com/SathwikGM/MyPortfolio",
+  },
+  {
+    name: "My Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quibusdam, doloremque repudiandae ratione autem tenetur illo. Repellendus eius tempora dolore similique, quia reprehenderit harum repudiandae molestiae ad sit atque odit.",
+    pageUrl: "http://localhost:3000/about",
+    gitHubUrl: "https://github.com/SathwikGM/MyPortfolio",
+  },
+  {
+    name: "My Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quibusdam, doloremque repudiandae ratione autem tenetur illo. Repellendus eius tempora dolore similique, quia reprehenderit harum repudiandae molestiae ad sit atque odit.",
+    pageUrl: "http://localhost:3000/about",
+    gitHubUrl: "https://github.com/SathwikGM/MyPortfolio",
+  },
+  {
+    name: "My Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quibusdam, doloremque repudiandae ratione autem tenetur illo. Repellendus eius tempora dolore similique, quia reprehenderit harum repudiandae molestiae ad sit atque odit.",
+    pageUrl: "http://localhost:3000/about",
+    gitHubUrl: "https://github.com/SathwikGM/MyPortfolio",
+  },
+];
+
 const Projects = () => {
   return (
     <>
-      <div className="projects-container"></div>
+      <div className="projects-container">
+        <ul>
+          {projects.map((project) => (
+            <li key={project.name}>
+              {project.name}
+              {project.description}
+              <a href={project.gitHubUrl}>{project.name}</a>
+              <a href={project.gitHubUrl}>GitHub</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
